c64c63adb6ec9fa34e7c6b6bc7521de7691ed09d
semontoyava <semontoyava@unal.edu.co>
2025-05-16 16:14:14 -0500

    Tarea_2_final

---
+++ homework/tests/test_migracion.py
@@ -0,0 +1,17 @@
+import os
+
+
+def test_migracion():
+    filepath = "data/output/results.tsv"
+
+    if not os.path.exists(filepath):
+        raise FileNotFoundError(f"El archivo {filepath} no existe")
+
+    results = {}
+    with open(filepath, "r", encoding="utf-8") as f:
+        for line in f:
+            key, value = line.strip().split("\t")
+            results[key] = value
+
+    assert results.get("computational") == "3"
+    assert results.get("analytics") == "5"
